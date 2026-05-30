5c597c093fe9b609aed57acb1a9d728df3f34308
Yosshy <Yosshy_123@proton.me>
2026-05-31 07:00:53 +0900

    Delete choco-chat-refactored directory

--- choco-chat-refactored/public/js/init.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-// Socket.IO の初期化（他のスクリプトより先に読み込むこと）
-const socket = io(location.origin, {
-  transports:           ['websocket', 'polling'],
-  reconnection:         true,
-  reconnectionAttempts: Infinity,
-});
